c19b1512c1d81b1a3ec772afddba84f67a255855
Yubraj <imyubraz@gmail.com>
2023-05-27 21:35:08 +0545

    Initial commit :)

---
+++ src/index.js
@@ -0,0 +1,68 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var path_1 = require("path");
+var cors_1 = require("cors");
+var app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+app.use("/images", express_1.default.static(path_1.default.join(__dirname, "../public/images")));
+app.get("/", function (req, res) {
+    return res.status(200).json({
+        success: true,
+        message: "Backend working!"
+    });
+});
+app.get("/foods", function (req, res) {
+    console.log(path_1.default.join(__dirname, "../public"));
+    var foodData = [
+        {
+            name: "Boilded Egg",
+            price: 10,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/egg.png",
+            type: "breakfast",
+        },
+        {
+            name: "RAMEN",
+            price: 25,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/ramen.png",
+            type: "lunch",
+        },
+        {
+            name: "GRILLED CHICKEN",
+            price: 45,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/chicken.png",
+            type: "dinner",
+        },
+        {
+            name: "CAKE",
+            price: 18,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/cake.png",
+            type: "breakfast",
+        },
+        {
+            name: "BURGER",
+            price: 23,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/burger.png",
+            type: "lunch",
+        },
+        {
+            name: "PANCAKE",
+            price: 25,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/pancake.png",
+            type: "dinner",
+        },
+    ];
+    res.json(foodData);
+});
+var PORT = 8000;
+app.listen(PORT, function () {
+    console.log("Server is running on port ".concat(PORT));
+});
